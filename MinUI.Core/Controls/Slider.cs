3e524e6d70daba0cbf76a25534b314fdd9cb1298
hbm <hbmin3789@naver.com>
2024-06-13 18:03:49 +0900

    [ADD] add SliderBar

--- MinUI.Core/Controls/Slider.cs
+++ MinUI.Core/Controls/Slider.cs
@@ -5,19 +5,47 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace MinUI.Core;
 
 public class Slider : ContentControl
 {
+    private readonly string _sliderButtonName = "SliderButton";
+    private readonly string _sliderBarName = "SliderBar";
+    private readonly string _sliderBackgroundName = "SliderBackground";
+    private System.Windows.Controls.Button _sliderButton;
+    private Border _sliderBar;
+    private Border _sliderBackground;
+    private Point _previousPosition;
+
     #region Dependency Properties
     public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
     nameof(Value), typeof(double), typeof(Slider), new FrameworkPropertyMetadata(default(double), FrameworkPropertyMetadataOptions.AffectsArrange));
+
     public double Value
     {
         get => (double)GetValue(ValueProperty);
         set => SetValue(ValueProperty, value);
+    }
+
+    public static readonly DependencyProperty MaxValueProperty = DependencyProperty.Register(
+    nameof(MaxValue), typeof(double), typeof(Slider), new FrameworkPropertyMetadata(100.0d, FrameworkPropertyMetadataOptions.AffectsArrange));
+
+    public double MaxValue
+    {
+        get => (double)GetValue(MaxValueProperty);
+        set => SetValue(MaxValueProperty, value);
+    }
+
+    public static readonly DependencyProperty MinValueProperty = DependencyProperty.Register(
+    nameof(MinValue), typeof(double), typeof(Slider), new FrameworkPropertyMetadata(0.0d, FrameworkPropertyMetadataOptions.AffectsArrange));
+
+    public double MinValue
+    {
+        get => (double)GetValue(MinValueProperty);
+        set => SetValue(MinValueProperty, value);
     }
 
     public static readonly DependencyProperty ProgressBarColorProperty = DependencyProperty.Register(
@@ -28,5 +56,71 @@ public class Slider : ContentControl
         get => (SolidColorBrush)GetValue(ProgressBarColorProperty);
         set => SetValue(ProgressBarColorProperty, value);
     }
+
+    public static readonly DependencyProperty ToolTipVisibilityProperty = DependencyProperty.Register(
+    nameof(ToolTipVisibility), typeof(Visibility), typeof(Slider), new FrameworkPropertyMetadata(Visibility.Visible, FrameworkPropertyMetadataOptions.AffectsArrange));
+
+    public Visibility ToolTipVisibility
+    {
+        get => (Visibility)GetValue(ToolTipVisibilityProperty);
+        set => SetValue(ToolTipVisibilityProperty, value);
+    }
+
+    public static readonly DependencyProperty IsMouseDownProperty = DependencyProperty.Register(
+    nameof(IsMouseDown), typeof(bool), typeof(Slider), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsArrange));
+
+    public bool IsMouseDown
+    {
+        get => (bool)GetValue(IsMouseDownProperty);
+        set => SetValue(IsMouseDownProperty, value);
+    }
     #endregion
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        _sliderButton = GetTemplateChild(_sliderButtonName) as System.Windows.Controls.Button;
+        _sliderBar = GetTemplateChild(_sliderBarName) as Border;
+        _sliderBackground = GetTemplateChild(_sliderBackgroundName) as Border;
+        if (_sliderBackground != null && _sliderButton != null)
+        {
+            SetMouseUpTemplate();
+            _sliderButton.PreviewMouseDown += (object sender, MouseButtonEventArgs e) => SetMouseDownTemplate();
+            _sliderButton.PreviewMouseUp += (object sender, MouseButtonEventArgs e) => SetMouseUpTemplate();
+            _sliderBackground.MouseLeave += (object sender, MouseEventArgs e) => SetMouseUpTemplate();
+            _sliderBackground.PreviewMouseDown += OnMouseDown;
+            _sliderBackground.PreviewMouseUp += (object sender, MouseButtonEventArgs e) => SetMouseUpTemplate();
+        }
+        this.MouseMove += Slider_MouseMove;
+    }
+
+    private void Slider_MouseMove(object sender, MouseEventArgs e)
+    {
+        Point currentPosition = e.GetPosition(this);
+        if (IsMouseOver && IsMouseDown)
+        {
+            double prevX = (Width * Value) / (MaxValue - MinValue);
+            double deltaX = currentPosition.X - _previousPosition.X;
+            Value += (deltaX / Width) * (MaxValue - MinValue);
+        }
+        _previousPosition = currentPosition;
+    }
+
+    private void SetMouseDownTemplate()
+    {
+        IsMouseDown = true;
+        _sliderButton.Template = Application.Current.Resources["MouseDownSliderButtonTemplate"] as ControlTemplate;
+    }
+
+    private void SetMouseUpTemplate()
+    {
+        IsMouseDown = false;
+        _sliderButton.Template = Application.Current.Resources["DefaultSliderButtonTemplate"] as ControlTemplate;
+    }
+    private void OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        SetMouseDownTemplate();
+        var position = e.GetPosition(this);
+        Value = ((MaxValue - MinValue) / 100) * (position.X / (Width / 100));
+    }
 }
--- MinUI.Core/Converters/ValueToSliderWidthConverter.cs
+++ MinUI.Core/Converters/ValueToSliderWidthConverter.cs
@@ -8,29 +8,35 @@ using System.Windows.Data;
 
 namespace MinUI.Core.Converters
 {
-    public class ValueToSliderWidthConverter : IValueConverter
+    public class ValueToSliderWidthConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            Slider slider = (Slider)value;
-            double val = (double)slider.Value;
-            if (val <= 0)
+            try
+            {
+                var BackgroundWidth = (double)values[0];
+                double val = (double)values[1];
+                double maxVal = (double)values[2];
+                double minVal = (double)values[3];
+                double newWidth = (BackgroundWidth * val) / (maxVal - minVal);
+                if (val <= 0)
+                {
+                    return 0;
+                }
+                if (newWidth >= BackgroundWidth)
+                {
+                    return BackgroundWidth;
+                }
+                return newWidth;
+            }
+            catch
             {
                 return 0;
             }
-            double width = (slider.Width / 100) * val;
-            if (width <= slider.Height)
-            {
-                return slider.Height;
-            }
-            if (width >= 100)
-            {
-                return 100;
-            }
-            return width;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
