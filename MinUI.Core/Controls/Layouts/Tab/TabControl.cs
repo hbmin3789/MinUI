e2662e5452c4bb52d7946d4054ec8ce63bd91eeb
hbm <hbmin3789@naver.com>
2024-06-20 14:38:38 +0900

    [ADD] add TabControl type

--- MinUI.Core/Controls/Layouts/Tab/TabControl.cs
+++ MinUI.Core/Controls/Layouts/Tab/TabControl.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace MinUI.Core;
 
-public class TabControl : System.Windows.Controls.TabControl
+public class TabControl : NeumorphBase
 {
 
 }
